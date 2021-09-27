58d2286ff2928038f17955f002455fee65c92adb
Andrew Cutlip <snakeeye10@gmail.com>
2021-09-27 11:27:37 -0400

    Sets up docker to play nice with heroku

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,6 @@ COPY . .
 
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE $PORT
 
-ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
-RUN chmod +x /wait
-
-CMD /wait && python ./src/main.py
+CMD python ./src/main.py $PORT
--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+import sys
 
-def hello():
-    print("Hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello  World</p>"
+
+
+if __name__ == "__main__":
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    app.run(host="0.0.0.0", port=port)
