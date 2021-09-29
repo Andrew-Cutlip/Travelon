7c619bf20b34f30860dd184111c6c1fad54f2b83
Andrew Cutlip <snakeeye10@gmail.com>
2021-09-28 20:30:27 -0400

    Adds routes to server for task # 49

--- src/flask-server/app.py
+++ src/flask-server/app.py
@@ -1,7 +1,7 @@
 import sys
+from auth import *
 
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -14,6 +14,33 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route("/register")
+def register():
+    # got stuff!
+    error = []
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if is_username_valid(username) and is_password_valid(password):
+            # TODO register user
+            pass
+        else:
+            error = "Invalid username / password"
+    return "Registration page"
+
+
+@app.route("/login")
+def login():
+    # got stuff!
+    error = []
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # TODO look up username and see if password matches
+        pass
+    return "Login page"
 
 
 if __name__ == '__main__':
