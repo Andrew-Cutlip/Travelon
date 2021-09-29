e9179b3e0f5a3e924c889e7923d050b7bf5f97bb
JODGEW <wenhaohe@buffalo.edu>
2021-09-29 15:05:35 -0400

    Homepage needed in react #48

---
+++ src/server/static/feature.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class feature extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', project: ''};
+  }
+
+  render() {
+    return(
+        <>
+          <button>Join Now</button>
+        </>
+    )
+    }
+}
