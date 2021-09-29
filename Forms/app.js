3f07a887b4020266dced64b873bd1cd34c7ff461
najee <najeeborden@gmail.com>
2021-09-29 17:48:53 -0400

    FORMS

---
+++ Forms/app.js
@@ -0,0 +1,15 @@
+import RegistrationForm from "./Components/register";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationForm>
+
+      </RegistrationForm>
+
+    </div>
+  );
+}
+
+export default App;
