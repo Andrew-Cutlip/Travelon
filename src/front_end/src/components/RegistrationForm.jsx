28495b80ab022ef7a7cd1763b39140bb0d9226bf
JODGEW <wenhaohe@buffalo.edu>
2021-10-17 18:18:37 -0400

    Google button sign up task #103

--- src/front_end/src/components/RegistrationForm.jsx
+++ src/front_end/src/components/RegistrationForm.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import RedirectButton from "./RedirectButton"
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+
 function RegistrationForm(props) {
 
 
@@ -7,6 +9,27 @@ function RegistrationForm(props) {
         console.log("Submit Clicked")
         e.preventDefault();
     }
+
+    const clientId = "891542752976-2b518b2l45tl0foe8c17pjm1h76pcf79.apps.googleusercontent.com"
+    const [showLoginButton, setShowLoginButton] = useState(true);
+    const [showLogoutButton, setShowLogoutButton] = useState(false);
+
+    const onLogInSuccess = (res) => {
+        console.log('Login Success:', res.profileObj);
+        setShowLoginButton(false);
+        setShowLogoutButton(true);
+    }
+
+    const onFailureSuccess = (res) => {
+        console.log('Login Failed:', res);
+    }
+
+    const onSignoutSuccess = (res) => {
+        alert("You have been signed out successfully.")
+        setShowLoginButton(true);
+        setShowLogoutButton(false);
+    }
+
     return (
         <>
             <h1>Register</h1>
@@ -31,6 +54,26 @@ function RegistrationForm(props) {
            onclick={() => props.onclick("login")}>
 
            </RedirectButton>
+
+            {showLoginButton ?
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Login"
+                    onSuccess={onLogInSuccess}
+                    onFailure={onFailureSuccess}
+                    cookiePolicy={'single_host_origin'}
+                /> : null
+
+            }
+
+            {showLogoutButton ?
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Logout"
+                    onLogoutSuccess={onSignoutSuccess}
+                >
+                </GoogleLogout> : null
+            }
         </>
 
 
