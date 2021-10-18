971139ac42fcb44484c2a9a84bb6efdda32cfad1
Andrew Cutlip <snakeeye10@gmail.com>
2021-10-18 19:31:05 -0400

    Merged Registration form

--- src/front_end/src/components/RegistrationForm.jsx
+++ src/front_end/src/components/RegistrationForm.jsx
@@ -1,14 +1,39 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import RedirectButton from "./RedirectButton"
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
 
 function RegistrationForm(props) {
 
+    const [Password, setPassword] = useState("");
+    const [Username, setUsername] = useState("")
+    const [Email, setEmail] = useState("")
+    const [Submit, setSubmit] = useState(false)
+
+    useEffect(() => {
+       const requestOptions = {
+           method: 'POST',
+           headers: {
+               "Content-Type": "application/json"
+           },
+           body: JSON.stringify({
+               username: Username,
+               password: Password,
+               email: Email
+           })
+       };
+           if (Submit) {
+            fetch("/register", requestOptions)
+                .then(response => response.json)
+                .then(data => console.log(data))
+                .then(() => setSubmit(false))
+            }
+    }, [Submit, Password, Username, Email]);
+
 
     let handleSubmit = (e) => {
-        console.log("Submit Clicked")
         e.preventDefault();
-    }
+        setSubmit(true);
+    };
 
     const clientId = "891542752976-2b518b2l45tl0foe8c17pjm1h76pcf79.apps.googleusercontent.com"
     const [showLoginButton, setShowLoginButton] = useState(true);
@@ -19,33 +44,40 @@ function RegistrationForm(props) {
         setShowLoginButton(false);
         setShowLogoutButton(true);
     }
-
     const onFailureSuccess = (res) => {
         console.log('Login Failed:', res);
     }
-
     const onSignoutSuccess = (res) => {
         alert("You have been signed out successfully.")
         setShowLoginButton(true);
         setShowLogoutButton(false);
     }
 
+    let handleUsername = (e) => {
+        setUsername(e.target.value);
+    };
+    let handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    let handleEmail = (e) => {
+        setEmail(e.target.value);
+    };
     return (
         <>
             <h1>Register</h1>
-                <form id="Register" name="Register" onSubmit={handleSubmit} action="/register" method="POST">
+                <form id="Register" name="Register" onSubmit={handleSubmit}>
 
                         <label>
                             UserName:
-                            <input name="username"/>
+                            <input name="username" type="text" placeholder="Username" required onChange={handleUsername}/>
                         </label>
                         <label>
                             Password:
-                            <input name="password" type="password"/>
+                            <input name="password" type="password" required onChange={handlePassword}/>
                         </label>
                         <label>
                             Email
-                            <input name="email" type="email"/>
+                            <input name="email" type="email" required onChange={handleEmail}/>
                         </label>
                         <input type="submit" value="Submit"/>
 
