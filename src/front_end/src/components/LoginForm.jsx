a94ad65c74a0b87f54a91c8702f589b7842d1b18
najee <najeeborden@gmail.com>
2021-10-20 19:32:17 -0400

    Css Added to Login and Registration forms

--- src/front_end/src/components/LoginForm.jsx
+++ src/front_end/src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import RedirectButton from "./RedirectButton"
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
-import "./Form.css";
+import "./Form.css"; 
 
 function Login(props) {
     const clientId = "891542752976-2b518b2l45tl0foe8c17pjm1h76pcf79.apps.googleusercontent.com"
