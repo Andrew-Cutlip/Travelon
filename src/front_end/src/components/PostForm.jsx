4e506357d47e171bb7aea8784895c0cb9680bdde
Andrew Cutlip <snakeeye10@gmail.com>
2021-11-11 02:08:11 -0500

    Finishes form for #135

--- src/front_end/src/components/PostForm.jsx
+++ src/front_end/src/components/PostForm.jsx
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from "react";
 
 const PostForm = (props) => {
     const [Submit, setSubmit] = useState(false);
+    const [Title, setTitle] = useState("");
+    const [Summary, setSummary] = useState("");
+    const [Location, setLocation] = useState("");
     useEffect(() => {
         const requestOptions = {
             method: 'POST',
@@ -17,21 +20,38 @@ const PostForm = (props) => {
         }
     }, [Submit]);
 
+    const handleSubmit = () => {
+        setSubmit(true);
+    };
+
+    const handleTitle = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleSummary = (e) => {
+      setSummary(e.target.value);
+    };
+
+    const handleLocation = (e) => {
+        setLocation(e.target.value);
+    }
+
     return (
         <div>
             <form>
                 <label htmlFor="title">
-                    <input type="text" name="title" id="title" placeholder="Title"/>
+                    <input type="text" name="title" id="title" placeholder="Title" value={Title}
+                           onChange={handleTitle}/>
                 </label>
                 <label htmlFor="summary">
-                    <textarea name="summary" id="summary" placeholder="Summarize your trip here."/>
+                    <textarea name="summary" id="summary" placeholder="Summarize your trip here."
+                              value={Summary} onChange={handleSummary}/>
                 </label>
                 <label htmlFor="location">
-                    <input type="text" name="location" id="location" placeholder="Location"/>
+                    <input type="text" name="location" id="location" placeholder="Location" value={Location}
+                           onChange={handleLocation}/>
                 </label>
-                <fieldset>
-
-                </fieldset>
+                <btn  type="submit" onclick={handleSubmit}>Add Post</btn>
             </form>
 
         </div>
