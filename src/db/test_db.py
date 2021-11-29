dbc0e4adb9e4859e1a9516eb6532b675393f1196
Andrew Cutlip <snakeeye10@gmail.com>
2021-11-29 17:38:47 -0500

    Finishes Adding Validation for task #153

--- src/db/test_db.py
+++ src/db/test_db.py
@@ -46,14 +46,14 @@ class MyTestCase(unittest.TestCase):
 
     def test_friends2(self):
         db.RealDatabase().add_friend("Bob", "Charles")
-=======
+
     def test_restaurant(self):
         database = db.RealDatabase()
         db.RealDatabase.display_restaurant(database, "China Taste1")
         print(db.RealDatabase.display_restaurant(database, "China Taste1"))
         # db.RealDatabase.add_restaurants_rating(database, "China Taste1", 5, "bad!", "Jac")
         # db.RealDatabase.add_restaurants(database, "Woo Chon")
->>>>>>> develop
+
 
 
 if __name__ == '__main__':
--- src/server/validation.py
+++ src/server/validation.py
@@ -10,13 +10,31 @@ def validate_post(post: dict):
         image_check = check_post_images(images, user_posts)
         if not image_check:
             return False
+    ratings = post.get("ratings")
+    if ratings is not None:
+        ratings_check = check_post_ratings(ratings, user_posts)
+        if not ratings_check:
+            return False
+    return True
 
-               
+
 def check_post_images(images, user_posts):
     for image in images:
         for post in user_posts:
-            img_id = post.get("img_id")
-            if img_id is not None:
-                if img_id == image:
-                    return False
+            img_ids = post.get("img_ids")
+            if img_ids is not None:
+                for img_id in img_ids:
+                    if img_id == image:
+                        return False
+    return True
+
+
+def check_post_ratings(ratings, user_posts):
+    for rating in ratings:
+        for post in user_posts:
+            rating_ids = post.get("rating_ids")
+            if rating_ids is not None:
+                for rating_id in  rating_ids:
+                    if rating_id == rating:
+                        return False
     return True
