dc290f06e4e5c16217acdde00924ce26712b662f
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-01 17:54:12 -0500

    Fixes build script and commit built react files

--- BuildandMove.bash
+++ BuildandMove.bash
@@ -1,4 +1,6 @@
 # shellcheck disable=SC2164
 cd src/front_end
 npm run build
-cp -a build/* ../server
+rsync -av build/static/* ../server/static
+rsync -av build/index.html ../server/static
+# cp -a build/* ../server
